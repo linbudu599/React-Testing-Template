3fd8a254bd77b4ec9fc97453370b27a2722b7a13
linbudu599 <linbudu599@gmail.com>
2020-06-09 21:27:43 +0800

    :zap: chore: reorganize project category, fix conflict

--- comp.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Comp = () => {
-  return <p>芜湖!</p>;
-};
-
-export default Comp;
--- test/Component.test.tsx
+++ test/Component.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { render, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import MyComponent, { commonFunc, asyncFunc, data } from './Component';
+import MyComponent, { commonFunc, asyncFunc, data } from '../components/Component';
 
 Enzyme.configure({ adapter: new Adapter() });
 
