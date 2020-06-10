6db04bc134f1e6bdb5136d90bdbe374566e8c5cc
linbudu599 <linbudu599@gmail.com>
2020-06-10 21:00:16 +0800

    :white_check_mark: feat: useCounter hooks test case

---
+++ test/hooks/useCounter.test.ts
@@ -0,0 +1,15 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import useCounter from "../../src/hooks/useCounter";
+
+describe("should test customize hooks with no deps", () => {
+  it("should increment count", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.increment();
+    });
+
+    expect(result.current.count).toBe(1);
+  });
+});
